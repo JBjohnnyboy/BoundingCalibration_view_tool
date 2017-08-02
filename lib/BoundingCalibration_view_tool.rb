dcd3dd9711271464a0e9ab1b3e9e6f9b1d1cd2b0
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 17:02:17 +0000

    Dev tool initial commit

---
+++ lib/BoundingCalibration_view_tool.rb
@@ -0,0 +1,5 @@
+require "BoundingCalibration_view_tool/version"
+
+module BoundingCalibrationViewTool
+  # Your code goes here...
+end
---
+++ lib/BoundingCalibration_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BoundingCalibrationViewTool
+  VERSION = "0.1.0"
+end
